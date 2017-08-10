2a5988644938d717ef6ae880aa1b802b4c6c04af
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-08-10 10:57:22 +0200

    added fb settings

--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import '/imports/api/items';
-import FacebookOAuthInit from './imports/oauth-facebook'
+import FacebookOAuthInit from './imports/oauth-facebook';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  FacebookOAuthInit();
+  // FacebookOAuthInit();
 });
